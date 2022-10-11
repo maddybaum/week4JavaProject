abe387075f4fc45855816c5ed001ebbf8babba9c
maddybaum <madeline.baum94@gmail.com>
2022-10-11 17:18:31 -0500

    part 2 of project

---
+++ src/main/java/com/example/demo/dtos/UserDto.java
@@ -0,0 +1,36 @@
+package com.example.demo.dtos;
+
+import com.example.demo.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+
+    public class UserDto implements Serializable {
+    private Long id;
+    private String username;
+    private String password;
+    Set<NoteDto> NoteDtoSet = new HashSet<>();
+
+    public UserDto(User user){
+        if(user.getId() != null){
+            this.id = user.getId();
+        }
+        if(user.getUsername() != null){
+            this.username = user.getUsername();
+        }
+        if(user.getPassword() != null){
+            this.password = user.getPassword();
+        }
+    }
+
+}
---
+++ src/main/java/com/example/demo/entities/User.java
@@ -0,0 +1,65 @@
+package com.example.demo.entities;
+
+
+import com.example.demo.dtos.UserDto;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User() {
+    }
+    public User(UserDto userDto){
+        if(userDto.getUsername() != null){
+            this.username = userDto.getUsername();
+        } if (userDto.getPassword() != null){
+            this.password = userDto.getPassword();
+        }
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JsonManagedReference
+    private Set<Note> noteSet = new HashSet<>();
+}
