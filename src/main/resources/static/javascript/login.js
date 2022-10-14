f6006e8463ff7a468c8b0493941740521fc22da8
maddybaum <madeline.baum94@gmail.com>
2022-10-14 10:49:13 -0500

    added to JS files

--- src/main/resources/static/javascript/login.js
+++ src/main/resources/static/javascript/login.js
@@ -1,3 +1,13 @@
+let loginForm = document.getElementById('login-form')
+let loginUsername = document.getElementById('login-username')
+let loginPassword = document.getElementById('login-password')
+
+const headers = {
+    'Content-Type':'application/json'
+}
+
+const baseUrl = 'http://localhost:8080/api/v1/users'
+
 const handleSumbit = async (e) => {
 e.preventDefault()
 
